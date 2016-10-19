0c3f29ad9d86548c151a4777deae19b46f089072
Juan Pablo Marengo <juanpmarengo@gmail.com>
2016-10-19 20:22:10 -0300

    Running Tests in Tarjeta

--- src/test/java/poli/tarjetamovi/AppTest.java
+++ src/test/java/poli/tarjetamovi/AppTest.java
@@ -1,8 +1,10 @@
 package poli.tarjetamovi;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import java.time.LocalDateTime;
+
+import static poli.tarjetamovi.Constants.*;
 
 /**
  * Unit test for simple App.
@@ -10,8 +12,55 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-    public void testViajes()
-    {
+    private Tarjeta tarjeta;
+    private Colectivo colectivo;
+    private Bicicleta bicicleta;
 
+    protected void setUp(){
+        tarjeta = new Tarjeta();
+        colectivo = new Colectivo("144 Negro", "Rosario Bus");
+        bicicleta = new Bicicleta("Bicicleta", "EMR");
+    }
+
+    protected void tearDown(){
+        tarjeta = new Tarjeta();
+        colectivo = new Colectivo("144 Negro", "Rosario Bus");
+        bicicleta = new Bicicleta("Bicicleta", "EMR");
+    }
+
+    public void testTarjetaRecarga()
+    {
+        tarjeta.recargar(10.0);
+
+        assertEquals(10.0, tarjeta.getSaldo());
+
+        tarjeta.recargar(272.0);
+
+        assertEquals((10.0 + 272.0 + 48.0), tarjeta.getSaldo());
+
+        tarjeta.recargar(500.0);
+
+        assertEquals((330.0 + 500.0 + 140.0), tarjeta.getSaldo());
+    }
+
+    public void testTarjetaPago()
+    {
+        tarjeta.recargar(100.0);
+
+        tarjeta.pagar(colectivo, LocalDateTime.now(), NORMAL);
+
+        assertEquals(96.0, tarjeta.getSaldo());
+
+        tarjeta.pagar(colectivo, LocalDateTime.now(), MEDIO);
+
+        assertEquals(94.0, tarjeta.getSaldo());
+
+        tarjeta.pagar(colectivo, LocalDateTime.now(), GRATIS);
+
+        assertEquals(94.0, tarjeta.getSaldo());
+
+        tarjeta.pagar(bicicleta, LocalDateTime.now(), MEDIO);
+
+        assertEquals(82.0, tarjeta.getSaldo());
     }
 }
